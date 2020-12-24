d398959286ace299ab3ca9bcdc7f3caa69d45c2c
unknown <mortersen@163.com>
2020-12-24 22:53:53 +0800

    设置连环画功能

--- C35N.py
+++ C35N.py
@@ -1,12 +1,72 @@
 import sys
-from PyQt5.QtWidgets import QApplication,QMainWindow,QTabWidget,QWidget,QMessageBox,QHBoxLayout,QPushButton
+from PyQt5.QtWidgets import (
+    QApplication,QMainWindow,QTabWidget,QWidget,QMessageBox,QHBoxLayout,QPushButton,
+    QLabel,
+)
 from PyQt5.QtSql import QSqlDatabase
-from PyQt5.QtGui import QPalette,QPixmap,QBrush
+from PyQt5.QtGui import QPalette,QPixmap,QBrush,QPainter
+
+from UI.UI_MainWindow import Ui_mainWindow
+from UI.UI_ComicBook import Ui_comicBook
+import img_rc
+
+class ComicBook(QWidget,Ui_comicBook):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_comicBook()
+        self.ui.setupUi(self)
+
+        #载入连环画
+        self.picPath=[":/Icon/KJZ-C35N-1.png",":/Icon/KJZ-C35N-2.png",":/Icon/KJZ-C35N-3.png",
+                      ":/Icon/KJZ-C35N-4.png",":/Icon/KJZ-C35N-5.png",":/Icon/KJZ-C35N-6.png",
+                      ":/Icon/KJZ-C35N-7.png",":/Icon/KJZ-C35N-8.png",":/Icon/KJZ-C35N-9.png",
+                      ":/Icon/KJZ-C35N-10.png",":/Icon/KJZ-C35N-11.png",":/Icon/KJZ-C35N-12.png",
+                      ":/Icon/KJZ-C35N-13.png",":/Icon/KJZ-C35N-14.png",":/Icon/KJZ-C35N-15.png",
+                      ":/Icon/KJZ-C35N-16.png"]
+        self.currentPic = 0
+        self.ui.btn_PagePre.released.connect(self.prePage)
+        self.ui.btn_PageNext.released.connect(self.nextPage)
+
+    def prePage(self):
+        temp = self.currentPic - 1
+        if temp < 0 :
+            self.currentPic = 15
+        else:
+            self.currentPic = temp
+
+    def nextPage(self):
+        temp = self.currentPic + 1
+        if temp > 15:
+            self.currentPic = 0
+        else:
+            self.currentPic = temp
+
+    def paintEvent(self, e):
+        pic = QPixmap(self.picPath[self.currentPic]).scaled(self.ui.lab_PicShow.width(),self.ui.lab_PicShow.height())
+        self.ui.lab_PicShow.setPixmap(pic)
+        super().paintEvent(e)
+
+class MainWindow(QMainWindow,Ui_mainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.ui =  Ui_mainWindow()
+        self.ui.setupUi(self)
+        self.resize(1027,768)
+
+        self.comicBook = ComicBook()
 
 
+        #初始化数据库
+        self.DB = None
+        self.createDB()
 
-class MainWindow(QMainWindow):
+        self.ui.action_comicBook.triggered.connect(self.setComicBook)
 
+    def setComicBook(self):
+        self.setCentralWidget(self.comicBook)
+    def comicBookNextPage(self,strMessage):
+        print(strMessage)
 
     def createDB(self):
         if self.DB:
@@ -14,20 +74,10 @@ class MainWindow(QMainWindow):
         try:
             self.DB = QSqlDatabase.addDatabase("QSQLITE")
             self.DB.setDatabaseName("DB/C35N.db3")
-            if self.DB.open():
-                QMessageBox.information(self,"提示","成功链接数据库")
-            else:
-                QMessageBox.warning(self,"错误","数据库打开失败")
+            print("Open DB success!")
         except Exception as e:
             QMessageBox.critical(self,"错误","数据库驱动错误")
             print(e)
-
-    def paintEvent(self, e):
-        pix = self.backgroundPicture.scaled(self.Width(),self.Height())
-        palette = QPalette(self)
-        palette.setBrush(self.backgroundRole(),pix)
-        self.setPalette(palette)
-        super().paintEvent(e)
 
 if __name__ == '__main__':
     mainApp = QApplication(sys.argv)
