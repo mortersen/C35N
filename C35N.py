21485143641ca511dd1c1ee8b32d2c8fc2dc12cf
unknown <mortersen@163.com>
2020-12-24 10:47:55 +0800

    New Start

--- C35N.py
+++ C35N.py
@@ -1,23 +1,16 @@
 import sys
-from PyQt5.QtWidgets import QApplication,QMainWindow,QTabWidget,QWidget,QMessageBox
+from PyQt5.QtWidgets import QApplication,QMainWindow,QTabWidget,QWidget,QMessageBox,QHBoxLayout,QPushButton
 from PyQt5.QtSql import QSqlDatabase
+from PyQt5.QtGui import QPalette,QPixmap,QBrush
 
-from UI.MainWindow import Ui_MainWindow
-from UI.IndexWidget import Ui_Index
 
-class TabIndex(QWidget,Ui_Index):
-    def __init__(self):
-        super().__init__()
-        self.setupUi(self)
 
-class MainWindow(QMainWindow,Ui_MainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setupUi(self)
-        self.tabWidget.addTab(TabIndex(),"起始页")
-        self.createDB()
+class MainWindow(QMainWindow):
+
 
     def createDB(self):
+        if self.DB:
+            return
         try:
             self.DB = QSqlDatabase.addDatabase("QSQLITE")
             self.DB.setDatabaseName("DB/C35N.db3")
@@ -28,6 +21,13 @@ class MainWindow(QMainWindow,Ui_MainWindow):
         except Exception as e:
             QMessageBox.critical(self,"错误","数据库驱动错误")
             print(e)
+
+    def paintEvent(self, e):
+        pix = self.backgroundPicture.scaled(self.Width(),self.Height())
+        palette = QPalette(self)
+        palette.setBrush(self.backgroundRole(),pix)
+        self.setPalette(palette)
+        super().paintEvent(e)
 
 if __name__ == '__main__':
     mainApp = QApplication(sys.argv)
